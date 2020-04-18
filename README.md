74386dd507d08e9d05493d7316bc8fa7cdbb99b0
Michal Borowski <unpolacoo@gmail.com>
2020-04-18 13:18:48 +0200

    Add readme file

---
+++ README.md
@@ -0,0 +1,6 @@
+# Liquid transition effect
+
+> Simple example of liquid transition effect made with [GSAP Library](https://greensock.com/)
+Hover on image to see an animation.
+
+## [LIVE version]( https://unpolaco.github.io/Liquid-transition/)
--- app.js
+++ app.js
@@ -2,20 +2,20 @@
 new hoverEffect({
   parent: document.querySelector(".distortion"),
   intensity: 0.2,
-  image1: "/img/img3.jpg",
-  image2: "/img/img2.jpg",
+  image1: "./img/img3.jpg",
+  image2: "./img/img2.jpg",
   angle: Math.PI / 16,
   speedIn: 1,
   speedOut: 3,
-  displacementImage: "/img/diss.png"
+  displacementImage: "./img/diss.png"
 })
 new hoverEffect({
   parent: document.querySelector(".distortion2"),
   intensity: 0.2,
-  image1: "/img/s1.jpg",
-  image2: "/img/s3.jpg",
+  image1: "./img/s1.jpg",
+  image2: "./img/s3.jpg",
   angle: Math.PI / 16,
   speedIn: 1,
   speedOut: 3,
-  displacementImage: "/img/diss.png"
+  displacementImage: "./img/diss.png"
 })
