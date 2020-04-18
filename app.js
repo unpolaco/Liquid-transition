4f67cb20409e1ac2641205661328229d6ac94830
Michal Borowski <unpolacoo@gmail.com>
2020-04-18 12:42:06 +0200

    Initial commit

---
+++ app.js
@@ -0,0 +1,21 @@
+
+new hoverEffect({
+  parent: document.querySelector(".distortion"),
+  intensity: 0.2,
+  image1: "/img/img3.jpg",
+  image2: "/img/img2.jpg",
+  angle: Math.PI / 16,
+  speedIn: 1,
+  speedOut: 3,
+  displacementImage: "/img/diss.png"
+})
+new hoverEffect({
+  parent: document.querySelector(".distortion2"),
+  intensity: 0.2,
+  image1: "/img/s1.jpg",
+  image2: "/img/s3.jpg",
+  angle: Math.PI / 16,
+  speedIn: 1,
+  speedOut: 3,
+  displacementImage: "/img/diss.png"
+})
